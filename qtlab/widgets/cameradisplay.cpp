2eb393e4f8ca34a0becad8da288520b8c5220a86
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2020-04-20 17:42:10 +0200

    Add widgets

---
+++ qtlab/widgets/cameradisplay.cpp
@@ -0,0 +1,278 @@
+#include <QHBoxLayout>
+#include <QCheckBox>
+#include <QDirIterator>
+#include <QStack>
+#include <QRegularExpression>
+#include <QDialog>
+#include <QContextMenuEvent>
+#include <QPair>
+
+#include <qwt_slider.h>
+#include <qwt_plot_zoomer.h>
+#include <qwt_plot_marker.h>
+
+#include <qtlab/widgets/aspectratiowidget.h>
+#include <qtlab/widgets/colormaps.h>
+#include <qtlab/widgets/customspinbox.h>
+
+#include <qtlab/widgets/cameraplot.h>
+#include <qtlab/widgets/cameradisplay.h>
+
+CameraDisplay::CameraDisplay(QWidget *parent) :
+    QWidget(parent)
+{
+    setupUi();
+
+    vec.resize(2048 * 2048);
+}
+
+CameraDisplay::~CameraDisplay()
+{
+}
+
+void CameraDisplay::setLUTPath(QString value)
+{
+    LUTPath = value;
+    setupLUTmenu();
+}
+
+/**
+ * @brief CameraDisplay::getBuffer
+ * @return A pointer to the internal buffer for data to be plotted
+ */
+
+double *CameraDisplay::getBuffer()
+{
+    return vec.data();
+}
+
+void CameraDisplay::replot()
+{
+    plot->setData(vec);
+}
+
+void CameraDisplay::contextMenuEvent(QContextMenuEvent *event)
+{
+    event->accept();
+    menu->exec(QCursor::pos());
+}
+
+QString CameraDisplay::getTitle() const
+{
+    return title;
+}
+
+void CameraDisplay::setTitle(const QString &value)
+{
+    title = value;
+}
+
+void CameraDisplay::setupUi()
+{
+    QPair<double, double> *currentRange = new QPair<double, double>(0, 65535);
+
+    menu = new QMenu();
+    setContextMenuPolicy(Qt::DefaultContextMenu);
+
+    plot = new CameraPlot();
+    QwtPlotZoomer *zoomer = new QwtPlotZoomer(plot->canvas());
+    zoomer->setRubberBandPen(QColor(Qt::green));
+    zoomer->setTrackerPen(QColor(Qt::green));
+
+    QwtPlotMarker *cursorMarker = new QwtPlotMarker();
+    cursorMarker->setLineStyle(static_cast<QwtPlotMarker::LineStyle>(QwtPlotMarker::VLine | QwtPlotMarker::HLine));
+    cursorMarker->setLinePen(Qt::green);
+    cursorMarker->setValue(1024, 1024);
+    cursorMarker->attach(plot);
+    cursorMarker->setVisible(false);
+
+    QAction *autoscaleAction = new QAction("Autoscale");
+    autoscaleAction->setCheckable(true);
+    autoscaleAction->setChecked(true);
+
+    QDialog *dialog = new QDialog(this);
+    dialog->setWindowTitle(title);
+
+    QwtSlider *minSlider = new QwtSlider(Qt::Horizontal, dialog);
+    QwtSlider *maxSlider = new QwtSlider(Qt::Horizontal, dialog);
+
+    minSlider->setScalePosition(QwtSlider::NoScale);
+    minSlider->setLowerBound(0);
+    minSlider->setUpperBound(65535);
+    minSlider->setValue(currentRange->first);
+    minSlider->setEnabled(false);
+    minSlider->setMinimumWidth(300);
+
+    maxSlider->setScalePosition(QwtSlider::NoScale);
+    maxSlider->setLowerBound(0);
+    maxSlider->setUpperBound(65535);
+    maxSlider->setValue(currentRange->second);
+    maxSlider->setEnabled(false);
+
+    DoubleSpinBox *minSpinBox = new DoubleSpinBox();
+    minSpinBox->setRange(minSlider->minimum(), minSlider->maximum());
+    minSpinBox->setDecimals(0);
+    minSpinBox->setEnabled(false);
+
+    DoubleSpinBox *maxSpinBox = new DoubleSpinBox();
+    maxSpinBox->setRange(maxSlider->minimum(), maxSlider->maximum());
+    maxSpinBox->setDecimals(0);
+    maxSpinBox->setValue(maxSlider->maximum());
+    maxSpinBox->setEnabled(false);
+
+    QGridLayout *grid = new QGridLayout();
+    int row = 0;
+    int col = 0;
+    grid->addWidget(minSlider, row, col++);
+    grid->addWidget(minSpinBox, row++, col++);
+    col = 0;
+    grid->addWidget(maxSlider, row, col++);
+    grid->addWidget(maxSpinBox, row++, col++);
+
+    QCheckBox *autoscaleCheckBox = new QCheckBox("Autoscale");
+    autoscaleCheckBox->setChecked(autoscaleAction->isChecked());
+    connect(autoscaleCheckBox, &QCheckBox::clicked,
+            autoscaleAction, &QAction::trigger);
+
+    QVBoxLayout *vLayout = new QVBoxLayout();
+    vLayout->addLayout(grid);
+    vLayout->addWidget(autoscaleCheckBox);
+    dialog->setLayout(vLayout);
+
+    std::function<void(void)> updatePlotRange = [ = ](){
+        plot->setInterval(Qt::ZAxis, currentRange->first, currentRange->second);
+    };
+
+    connect(minSlider, &QwtSlider::valueChanged, this, [ = ](double value){
+        currentRange->first = value;
+        minSpinBox->setValue(value);
+        updatePlotRange();
+    });
+
+    connect(maxSlider, &QwtSlider::valueChanged, this, [ = ](double value){
+        currentRange->second = value;
+        maxSpinBox->setValue(value);
+        updatePlotRange();
+    });
+
+    connect(minSpinBox, &DoubleSpinBox::returnPressed, this, [ = ](double value){
+        currentRange->first = value;
+        minSlider->setValue(value);
+        updatePlotRange();
+    });
+
+    connect(maxSpinBox, &DoubleSpinBox::returnPressed, this, [ = ](double value){
+        currentRange->second = value;
+        maxSlider->setValue(value);
+        updatePlotRange();
+    });
+
+    connect(autoscaleAction, &QAction::triggered, this, [ = ](bool checked){
+        plot->setZAutoscaleEnabled(checked);
+        if (!checked) {
+            updatePlotRange();
+        }
+        autoscaleCheckBox->setChecked(checked);
+
+        minSlider->setEnabled(!checked);
+        maxSlider->setEnabled(!checked);
+        minSpinBox->setEnabled(!checked);
+        maxSpinBox->setEnabled(!checked);
+    });
+    menu->addAction(autoscaleAction);
+
+    QAction *action;
+    action = new QAction("Set Z range...");
+    connect(action, &QAction::triggered, dialog, &QDialog::show);
+    menu->addAction(action);
+
+    menu->addSeparator();
+
+    action = new QAction("Zoom out", this);
+    connect(action, &QAction::triggered, this, [ = ](){
+        zoomer->zoom(-1);
+    });
+    menu->addAction(action);
+
+    action = new QAction("Zoom in", this);
+    connect(action, &QAction::triggered, this, [ = ](){
+        zoomer->zoom(1);
+    });
+    menu->addAction(action);
+
+    action = new QAction("Reset zoom", this);
+    connect(action, &QAction::triggered, this, [ = ](){
+        zoomer->zoom(0);
+    });
+    menu->addAction(action);
+
+    menu->addSeparator();
+
+    action = new QAction("Show cross", this);
+    action->setCheckable(true);
+    action->setChecked(false);
+    connect(action, &QAction::triggered, this, [ = ](bool checked){
+        cursorMarker->setVisible(checked);
+        plot->replot();
+    });
+    menu->addAction(action);
+
+    LUTMenu = new QMenu("LUT");
+
+    setupLUTmenu();
+
+    menu->addMenu(LUTMenu);
+
+    QWidget *aspectRatioWidget = new AspectRatioWidget(plot, 1.1, 1., -80, -50);
+
+    QBoxLayout *layout = new QHBoxLayout();
+    layout->addWidget(aspectRatioWidget, 1);
+
+    setLayout(layout);
+}
+
+void CameraDisplay::setupLUTmenu()
+{
+    LUTMenu->clear();
+    QAction *action = new QAction("Grayscale");
+    LUTMenu->addAction(action);
+    connect(action, &QAction::triggered, this, [ = ](){
+        plot->setColorMap(new GrayScaleColorMap());
+    });
+
+    action = new QAction("Black Blue White");
+    LUTMenu->addAction(action);
+    connect(action, &QAction::triggered, this, [ = ](){
+        plot->setColorMap(new BlueWhiteColorMap());
+    });
+
+    action = new QAction("Hi Low");
+    LUTMenu->addAction(action);
+    connect(action, &QAction::triggered, this, [ = ](){
+        plot->setColorMap(new HiLowColorMap());
+    });
+
+    LUTMenu->addSeparator();
+
+    QDirIterator it(LUTPath,
+                    QStringList() << "*.lut",
+                    QDir::NoFilter, QDirIterator::Subdirectories);
+    QStringList sl;
+    while (it.hasNext()) {
+        sl << it.next();
+    }
+    sl.sort();
+
+    QStringListIterator sli(sl);
+    while (sli.hasNext()) {
+        QString path = sli.next();
+        QString name = path;
+        QAction *action = new QAction(name.remove(LUTPath)
+                                      .remove(QRegularExpression("\\.lut$")));
+        LUTMenu->addAction(action);
+
+        connect(action, &QAction::triggered, this, [ = ](){
+            plot->setColorMap(new IJLUTColorMap(path));
+        });
+    }
+}
---
+++ qtlab/widgets/cameradisplay.h
@@ -0,0 +1,44 @@
+#ifndef CAMERADISPLAY_H
+#define CAMERADISPLAY_H
+
+#include <QWidget>
+#include <QMenu>
+
+class CameraPlot;
+
+
+class CameraDisplay : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit CameraDisplay(QWidget *parent = nullptr);
+    virtual ~CameraDisplay();
+
+    void setLUTPath(QString value);
+    double *getBuffer();
+
+    QString getTitle() const;
+    void setTitle(const QString &value);
+
+signals:
+
+public slots:
+    void replot();
+
+protected:
+    virtual void contextMenuEvent(QContextMenuEvent *event);
+
+private:
+    CameraPlot *plot;
+    QVector<double> vec;
+    QMenu *menu;
+    QMenu *LUTMenu;
+
+    QString LUTPath;
+    QString title;
+
+    void setupUi();
+    void setupLUTmenu();
+};
+
+#endif // CAMERADISPLAY_H
